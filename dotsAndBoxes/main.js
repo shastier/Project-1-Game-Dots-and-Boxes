89986c78f06df5fe492a6896621725ac784aa26a
Shirley Hastier <shastier@yahoo.com>
2020-07-21 12:57:17 -0500

    Presentation screen design, setGrid(), classes Grid & Box

--- dotsAndBoxes/main.js
+++ dotsAndBoxes/main.js
@@ -1,7 +1,7 @@
 class GameBoard {
     constructor(){
         this.players = [];
-        this.grid = null;
+        this.grid = [];
     };
     getPlayers(){
         return this.players;
@@ -10,6 +10,12 @@ class GameBoard {
         if(this.players.length<2){
             this.players.push(player);            
         }
+    };
+    getGrid(){
+        return this.grid;
+    };
+    setGrid(grid){
+        this.grid.push(grid);
     };
     play(){
         alert("Time to play!");
@@ -24,6 +30,9 @@ class Player {
         this.color = 'black';
         this.points = 0;
     };
+    getName(){
+        return this.name;
+    };
     setColor(newColor){
         this.color = newColor;
     };
@@ -37,21 +46,80 @@ class Player {
         this.points += point;
     };
 }
+class Grid {
+    constructor(row, column){
+        this.row = row;
+        this.column = column;
+        this.boxes = [];
+        this.lastNumBoxes = 0;
+    };
+    getRow(){
+        return this.row;
+    };
+    getColumn(){
+        return this.column;
+    };
+    drawLine(player){
+        alert('Draw Line');
+        // if there is a new box, addBox
+    };
+    getNewBox(){
+        if(this.boxes.length === this.lastNumBoxes){
+            return null;
+        }else{
+            this.lastNumBoxes ++;
+            return this.boxes[this.boxes.length-1];
+        }
+    };
+    getBoxes(){
+        return this.boxes;
+    };
+}
+class Box {
+
+}
 const gameBoard = new GameBoard();
 
-function setPlayer(e) {  
-    const playerNumber = e.target.placeholder;
+function setPlayer(e) {    
     let player = new Player(e.target.value);
+    const playerNumber = e.target.placeholder;
 
     if (playerNumber === 'Player 1') {
         player.setColor('blue');
+        document.getElementById('player1').removeEventListener('input', setPlayer);
     }else{
         player.setColor('green');
+        document.getElementById('player2').removeEventListener('input', setPlayer);
     }    
     gameBoard.setPlayer(player);    
 };
-
+function setGrid(e){
+    const gridSize = parseInt(e.target.id.charAt(e.target.id.length-1));
+    const grid = new Grid(gridSize, gridSize);
+    
+    gameBoard.setGrid(grid);
+    
+    if (gameBoard.players.length < 2) {
+        if (gameBoard.players.length === 0) {
+            let player1 = new Player("Player 1");
+            let player2 = new Player("Player 2");
+            
+            player1.setColor('blue');
+            player2.setColor('green');
+            gameBoard.setPlayer(player1);
+            gameBoard.setPlayer(player2);
+        } else {
+            let player2 = new Player("Player 2");
+            player2.setColor('red');        
+            gameBoard.setPlayer(player2);
+        }
+    }   
+    //update screen to wireframe b.
+};
 window.onload = function(){    
     document.querySelector('#player1').addEventListener('input', setPlayer);
-    document.querySelector('#player2').addEventListener('input', setPlayer);    
+    document.querySelector('#player2').addEventListener('input', setPlayer);  
+    document.querySelector('#x3').addEventListener('click', setGrid);
+    document.querySelector('#x4').addEventListener('click', setGrid);
+    document.querySelector('#x5').addEventListener('click', setGrid);
 };
